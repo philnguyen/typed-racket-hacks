06e0b1070cc5ea171ff1b96493029518979f878d
Phil Nguyen <pcn@cs.umd.edu>
2018-03-05 20:31:10 -0500

    more

--- typed-racket-hacks/main.rkt
+++ typed-racket-hacks/main.rkt
@@ -1,6 +1,26 @@
 #lang typed/racket/base
 
-(require typed/racket/unsafe)
+(require typed/racket/unsafe
+         racket/set
+         set-extras)
 
 (unsafe-require/typed/provide racket/base
   [procedure-rename (∀ (B A ...) (A ... → B) Symbol → (A ... → B))])
+
+(provide
+ ;; TR for some reason only have obsolete names for set comprehensions
+ (rename-out [for/set: for/set]
+             [for*/set: for*/set])
+ ;; Missing these for some reasons
+ for/seteq
+ for*/seteq)
+
+(define-syntax-rule (make-form name fold-form)
+  (define-syntax name
+    (syntax-rules (:)
+      [(_ : T (for-clauses (... ...)) body (... ...))
+       (fold-form ([acc : T ∅eq]) (for-clauses (... ...))
+         (set-add acc (let () body (... ...))))])))
+
+(make-form for/seteq for/fold)
+(make-form for*/seteq for*/fold)
--- typed-racket-hacks/tests.rkt
+++ typed-racket-hacks/tests.rkt
@@ -3,9 +3,36 @@
 (require "main.rkt")
 
 (module+ test
+  (require (only-in racket/set set seteq)
+           typed/rackunit)
+  
   (define (id x) x)
   (define id1 (procedure-rename id 'identity))
   (id1 42)
 
   (define (sec x y) y)
-  ((procedure-rename sec 'second) 42 43))
+  ((procedure-rename sec 'second) 42 43)
+
+  (check-equal? (for/set : (Setof Integer) ([x '(1 2 3)])
+                  (add1 x))
+                (set 2 3 4))
+
+  (check-equal? (for*/set : (Setof (Pairof Integer Symbol)) ([x '(1 2 3)]
+                                                             [c '(a b)])
+                  (cons x c))
+                (set '(1 . a) '(1 . b)
+                     '(2 . a) '(2 . b)
+                     '(3 . a) '(3 . b)))
+  
+  ;; Get around chaperones from `check-equal?`
+  (check-true (equal? (for/seteq : (Setof Integer) ([x '(1 2 3)])
+                        (add1 x))
+                      (seteq 2 3 4)))
+
+  (check-true (equal? (for*/seteq : (Setof Integer) ([x '(1 2 3)]
+                                                     [y '(4 5)])
+                        (* x y))
+                      (seteq 4 5
+                             (* 2 4) (* 2 5)
+                             (* 3 4) (* 3 5))))
+  )
